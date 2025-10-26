6fbce2e4b5df23f4b37d3cc635e8c5c849a37d92
Travis Tran <travistran544@gmail.com>
2025-10-26 11:21:15 -0400

    Initial Commit

---
+++ OKNSafeRide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OKNSafeRide
+//
+//  Created by Travis Tran on 10/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OKNSafeRide/OKNSafeRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  OKNSafeRideApp.swift
+//  OKNSafeRide
+//
+//  Created by Travis Tran on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct OKNSafeRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
